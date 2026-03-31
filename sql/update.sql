40acee63e8fe4b5e1c390e4931ed0a56e86211b1
Ana Castellano <anacastellanorubio@gmail.com>
2026-03-31 19:39:16 +0200

    CAMBIAR DATOS SQL

---
+++ sql/update.sql
@@ -0,0 +1,9 @@
+---Cambiar el email de Laura por lauratest@gmail.com
+UPDATE form
+SET email = 'lauratest@gmail.com'
+Where nombre = 'Laura';
+
+---Cambiar edad 20 por 21
+UPDATE form
+SET edad= 20
+Where edad= 21;
