aa433f81271d6ffe144b46d38bf0b026aa206493
Ana Castellano <anacastellanorubio@gmail.com>
2026-04-19 00:00:53 +0200

    Modificacion Url Supabase

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // 1. CAMBIAR POR URL Y KEY DE TU API
-const SUPABASE_URL = 'https://ifgzperqnxoomyvvytzr.supabase.co/rest/v1/form';
+const SUPABASE_URL ='https://bavsppsyyfzblwdvhrca.supabase.co/rest/v1/form';'
 const SUPABASE_API_KEY = 'sb_publishable_AYa4RIwBAu1c5ORvsjwCxw_3aOrL8_C';
+
 
 
 //ENVIAR DATOS DE UN FORMULARIO A UNA API CON POST
